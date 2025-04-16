c212872507e7b51e9e8757f215dda9a15c7843d4
ToasterBuilder <toasterbuilder@gmail.com>
2025-04-16 10:57:30 -0400

    Add Arm Support

--- ExoSkeletonVR/Assets/Scripts/Teleport Scene.cs
+++ ExoSkeletonVR/Assets/Scripts/Teleport Scene.cs
@@ -9,16 +9,9 @@ public class TeleportScene : MonoBehaviour
 
     public string sceneName;
 
-    // Start is called before the first frame update
     void Start()
     {
         GetComponent<Button>().onClick.AddListener(ChangeScene);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void ChangeScene()
