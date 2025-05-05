90ded81647d26ccf25b88ed1bec1b4b807c8bd45
Kevin <toasterbuilder@gmail.com>
2025-05-05 10:58:53 -0400

    Add drill proximity task
    - Add the task sphere
    - When drill and task sphere are overlapping collision boxes start and display timer
    - stop timer when not overlapping
    
    Co-Authored-By: RiChaarani <172090524+RiChaarani@users.noreply.github.com>

---
+++ ExoSkeletonVR/Assets/Scripts/OnProximity.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class OnProximity : MonoBehaviour
+{
+    public string requiredTag;
+    public GameObject radialTimerUI;
+    public Slider radialSlider;
+    public float fillDuration = 10f;
+
+    private bool isInProximity = false;
+    private float timer = 0f;
+    private Coroutine fadeCoroutine;
+
+    private void Start()
+    {
+        if (radialTimerUI != null)
+        {
+            radialTimerUI.SetActive(false);
+        }
+
+        if (radialSlider != null) {
+            radialSlider.value = 0f;
+        }
+    }
+
+    private void Update()
+    {
+        if (isInProximity && radialSlider != null)
+        {
+            timer += Time.deltaTime;
+            radialSlider.value = Mathf.Clamp01(timer / fillDuration);
+
+            if (radialSlider.value >= 1f)
+            {
+                Debug.Log("Timer completed.");
+                timer = 0f;
+                radialSlider.value = 0f;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(requiredTag))
+        {
+            if (!isInProximity)
+            {
+                isInProximity = true;
+                if (radialTimerUI != null)
+                {
+                    radialTimerUI.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(requiredTag))
+        {
+            isInProximity = false;
+            radialTimerUI.SetActive(false);
+        }
+    }
+}
